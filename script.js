462cac7c9bb426fad98dfec0b9d71f371fac384e
Akash Tiwari <124699923+akash07t@users.noreply.github.com>
2023-04-13 18:49:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,7 +17,12 @@ const library = [
 ];
 
 const numberOfBooksRead = () => {
-  // write your code here
+	var ans = 0;
+	for(i = 0; i<library.length; i++){
+		if(library[i].readingStatus === true)
+			ans++;
+	}
+  return ans;
 };
 
 // Do not change the code below
